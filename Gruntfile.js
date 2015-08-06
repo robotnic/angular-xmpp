c8a4ad8034f67a7bbde9e1dbd4efd9ac307ed5f2
bernhard <bz@datenkueche.com>
2015-08-06 20:36:11 +0200

    version number

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,7 +199,7 @@ module.exports = function ( grunt ) {
           '<%= html2js.common.dest %>', 
           'module.suffix' 
         ],
-        dest: '<%= compile_dir %>/assets/<%= pkg.name %>-<%= pkg.version %>.js'
+        dest: '<%= compile_dir %>/assets/<%= pkg.name %>.js'
       }
     },
 
