8616ecb38e0af07bfdfc1f44b51849b56134adca
bernhard <bz@datenkueche.com>
2015-08-17 01:18:57 +0200

    some geojson testing

--- src/app/buddycloud-media/item.js
+++ src/app/buddycloud-media/item.js
@@ -16,9 +16,26 @@ angular.module("BuddycloudMedia",[])
 .controller("BuddycloudMediaController",function($scope,$http,$sce){                
     $http.get($scope.metadata).then(function(response){
         $scope.meta=response.data;
+        console.log($scope.meta);
+        if($scope.meta.mimeType=="application/octet-stream"||$scope.meta.mimeType=="application/json"){   //should be application/json
+            $scope.loadData(response.data);
+        }
     },function(error){
         console.log(error);
     });
+
+    $scope.loadData=function(meta){
+        var url="https://buddycloud.org/api/"+meta.entityId+"/media/"+meta.id;
+        console.log(url);
+        $http.get(url).then(function(response){
+            console.log(url,response);
+            if(response.data.type=="Feature" || response.data.type=="FeatureCollection"){
+                $scope.geojson=response.data;
+            }
+        },function(error){
+            console.log(error);
+        });
+    }
 })
 
 .filter('embedUrl', function ($sce) {
